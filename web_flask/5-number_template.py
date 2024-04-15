13dc15859d78bd36ece3d35c258c1899b9ba215e
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-04-15 18:10:30 +0400

    Number template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -2,7 +2,7 @@
 """hello flask"""
 
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -28,6 +28,9 @@ def python(text):
 def number(n):
     return f"{n} is a number"
 
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+      return render_template('5-number.html', n=n)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
