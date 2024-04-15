05bd1078298b14c56d10dfa8e19de84e85223d53
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-04-15 17:53:52 +0400

    Is it a number?

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""hello flask"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello():
+        return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+      return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+      changed_var = text.replace("_", " ")
+      return changed_var
+
+@app.route("/python/<text>", strict_slashes=False)
+def python(text):
+      changed_word = text.replace("_", " ")
+      return f"Python {changed_word}"
+
+@app.route("/number/<n>", strict_slashes=False)
+def number(n):
+    return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
